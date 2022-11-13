5896e60234d2957bf8b015745c0a835d605dbeb6
Aleksandr <lepalovskiy2015@mail.ru>
2022-11-13 13:51:12 +0700

    finishCommit

--- Семинар/Домашняя работа/Program.cs
+++ Семинар/Домашняя работа/Program.cs
@@ -38,4 +38,9 @@ else
 8) Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до (N).
 */
 
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= number1; i++)
+    if (i % 2 == 0)
+Console.Write(i);
 
